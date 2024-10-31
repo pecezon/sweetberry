593da3e9b4559bcf5aa5c852d107072a116e9a39
diegopez <lopez.diego@cetys.edu.mx>
2024-10-30 22:36:42 -0700

    avances en nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-import { Box, Flex, HStack, IconButton, useDisclosure, Image, Link, Button } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  HStack,
+  IconButton,
+  useDisclosure,
+  Image,
+  Link,
+  Button,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import {getTest} from "../functions/test";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { MdClose } from "react-icons/md";
+import { getTest } from "../functions/test";
 
 const Navbar = () => {
-
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [test, setTest] = useState("");
 
   useEffect(() => {
-    getTest().then(data => {
+    getTest().then((data) => {
       setTest(data.message);
     });
   }, []);
@@ -18,40 +29,52 @@ const Navbar = () => {
       padding={4}
       justifyContent="space-between"
       alignItems="center"
+      border="1px solid black"
     >
       {/* Logo */}
-      <Box
-        w="30%"
-      >
+      <Link w="30%" href="https://www.youtube.com">
         <Image
           src={`${process.env.PUBLIC_URL}/images/brand/logo.png`}
           alt="logo"
-          maxWidth="350px"
+          maxWidth={{ lg: "225px", xl: "275px" }}
           minWidth="100px"
           w="100%"
           h="auto"
         ></Image>
-      </Box>
+      </Link>
 
-      {/* Links */}
+      {/* Links for desktop*/}
       <Box
         w="30%"
         display="flex"
         justifyContent="space-between"
+        fontSize={{ lg: "1rem", xl: "1.5rem" }}
+        hideBelow="md"
       >
-        <Link>
-          Home
-        </Link>
-        <Link>
-          Flavors
-        </Link>
-        <Link>
-          Reviews
-        </Link>
-        <Link>
-          Toppings
-        </Link>
+        <Link>Home</Link>
+        <Link>Flavors</Link>
+        <Link>Reviews</Link>
+        <Link>Toppings</Link>
+      </Box>
 
+      {/* Links for mobile*/}
+      <Box
+        position="fixed"
+        top="0"
+        left="0"
+        width="100%"
+        height="100%"
+        bgcolor="white"
+        zIndex="10"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        sx={{ display: isMenuOpen ? "flex" : "none" }}
+      >
+        <Link>Home</Link>
+        <Link>Flavors</Link>
+        <Link>Reviews</Link>
+        <Link>Toppings</Link>
       </Box>
 
       {/* Language toogle */}
@@ -59,9 +82,22 @@ const Navbar = () => {
         w="30%"
         display="flex"
         justifyContent="flex-end"
+        border="1px solid black"
       >
-        <Button>{test}</Button>
+        <Button size={["xs", "sm", "md"]}>Esp</Button>
       </Box>
+
+      {/*Ham menu */}
+      <IconButton
+        hideFrom={"md"}
+        display={"flex"}
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+        size="xs"
+        alignContent="center"
+        justifyContent="center"
+      >
+        {isMenuOpen ? <MdClose /> : <RxHamburgerMenu />}
+      </IconButton>
     </Flex>
   );
 };
---
+++ client/utils/colors.js
@@ -0,0 +1,9 @@
+const theme = {
+  hardPink: "#FF46AE",
+  lightPink: "#FFB9E0",
+  hardBlue: "#A1D7FF",
+  hardGreen: "#A7C900",
+  hardOrange: "#FFB400",
+};
+
+export default theme;
