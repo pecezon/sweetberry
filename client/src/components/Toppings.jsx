2d84998199266bfea7b9f16b5a19963d10e82bfc
diegopez <lopez.diego@cetys.edu.mx>
2024-11-09 17:02:05 -0800

    Toppings section created, modified VisitUs

---
+++ client/src/components/Toppings.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Toppings = () => {
+  return (
+    <div>
+      <h2>Toppings</h2>
+      {/* Add your toppings content here */}
+    </div>
+  );
+};
+
+export default Toppings;
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -3,20 +3,24 @@ import Nav from "../components/Nav";
 import Main from "../components/Main";
 import WhyUs from "../components/WhyUs";
 import VisitUs from "../components/VisitUs";
-import { useMediaQuery } from 'react-responsive'
+import { useMediaQuery } from "react-responsive";
 import Flavors from "../components/Flavors";
 
 const Landing = () => {
-  const isBigScreen = useMediaQuery({ query: '(min-width: 1280px)' })
+  const isBigScreen = useMediaQuery({ query: "(min-width: 1280px)" });
 
   return (
-    <>
+    <div
+      style={{
+        maxWidth: "3000px",
+      }}
+    >
       <Nav />
-      <Main mediaQuery={isBigScreen}/>
+      <Main mediaQuery={isBigScreen} />
       <WhyUs />
       <Flavors />
-      <VisitUs mediaQuery={isBigScreen}/>
-    </>
+      <VisitUs mediaQuery={isBigScreen} />
+    </div>
   );
 };
 
